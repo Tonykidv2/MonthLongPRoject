6d3944f48dc9f311128a09dd4eb6527e55738d3d
Anthony Ramoslebron <anthony.ramoslebron@ad.infosys.com>
2017-12-07 14:55:47 -0600

    Adding other files

--- WebApp/Controllers/EmployeeManagementController.cs
+++ WebApp/Controllers/EmployeeManagementController.cs
@@ -22,5 +22,7 @@ namespace WebApp.Controllers
         {
             return PartialView("ShowAllStates");
         }
+
+
     }
 }
--- WebApp/MyScripts/Services.js
+++ WebApp/MyScripts/Services.js
@@ -3,6 +3,7 @@
     this.getEmployees = function (id) {
         return $http.get("http://localhost:6086/api/employee/getall");
     };
+
     this.getStates = function (id) {
         var some = $http.get("http://localhost:6086/api/state/getall")
         return some;
